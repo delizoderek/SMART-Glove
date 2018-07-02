eb901efc6311fda2c70c18646dc7e0c6f37d6fb4
Derek DeLizo <delizoderek@gmail.com>
2018-07-02 16:07:06 -0700

    Plotted a parabola using the 3D plotting from matplotlib

--- GlovePlot.py
+++ GlovePlot.py
@@ -1,11 +1,20 @@
-from mpl_toolkits.mplot3d import axes3d
+import matplotlib as mpl
+import math
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
 import matplotlib.pyplot as plt
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+mpl.rcParams['legend.fontsize'] = 10
 
-x,y,z = [1,2,3,4,5,6,7,8,9,10],[10,9,8,7,6,5,4,3,2,1],[1,2,3,4,5,6,7,8,9,10]
-ax.plot_wireframe(x,y,z)
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+theta = np.linspace(-4 * np.pi, 4 * np.pi, 100)
+x = t = np.linspace(0,11,12)
+z = -1 * (t)*(t-10)
+ax.set_xlim([0,20])
+ax.set_ylim([0,20])
+ax.set_zlim([0,30])
+ax.plot(x, t, z, label='parametric curve')
+ax.legend()
 
 plt.show()
-
