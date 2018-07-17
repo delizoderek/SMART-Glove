c3960eddbcd9b958af96e05cba0f4ea0efcf7c31
Derek DeLizo <delizoderek@gmail.com>
2018-07-16 21:30:28 -0700

    Added a bluetooth selector prompt for choosing devices

--- BluetoothSetup.py
+++ BluetoothSetup.py
@@ -1,6 +1,13 @@
 import bluetooth
 import sys
 
+count = 0
+
 nearby_devices = bluetooth.discover_devices() 
 for bdaddr in nearby_devices:
-    print bluetooth.lookup_name( bdaddr )  
+    count += 1
+    print count, ". " ,bluetooth.lookup_name( bdaddr )
+
+selection = input("> ") - 1
+print "You have selected", bluetooth.lookup_name(nearby_devices[selection])
+bd_addr = nearby_devices[selection]
