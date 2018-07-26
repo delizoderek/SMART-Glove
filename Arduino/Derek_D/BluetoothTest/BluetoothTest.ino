8634b32fa7a3a9a07cc3686b4846a919938747bf
Derek DeLizo <delizoderek@gmail.com>
2018-07-25 21:22:50 -0700

    Updated BluetoothTest.ino and BluetoothSetup.py.
    BluetoothTest.ino:
    - Will wait for laptop to connect over bluetooth before sending data
    - Is able to read in data from IMU
    - Format the x,y,z acceleration into a char array
    - Send the character array to the laptop
    
    BluetoothSetup.py:
    - Sends a confirmation string to arduino to confirm bluetooth link
    - Reads in bluetooth data until keyboard interrupt occurs
    - Combines the data bytes into a character list
    - When the char line end arrives appends the character list to the recordedData list
    - After keyboard interrupt occurs recorded data is written to a CSV file

--- Arduino/Derek_D/BluetoothTest/BluetoothTest.ino
+++ Arduino/Derek_D/BluetoothTest/BluetoothTest.ino
@@ -1,52 +1,60 @@
 /*
- * Created by Derek DeLizo
- * TX -> D2
- * RX -> D3
+   Created by Derek DeLizo
+   TX -> D2
+   RX -> D3
 */
 #include <SoftwareSerial.h>
+#include<Wire.h>
+const int MPU_addr = 0x68; // I2C address of the MPU-6050
+int16_t AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
 int baudrate = 9600;
 int i = 0;
-SoftwareSerial mySerial(2,3);
+bool isLinked;
+SoftwareSerial mySerial(2, 3);
 
-int data[17][6] = {{0,0,0,0,0,0},
-                {0,0,0,0,0,1},
-                {0,0,0,0,1,0},
-                {0,0,0,0,1,1},
-                {0,0,0,1,0,0},
-                {0,0,0,1,0,1},
-                {0,0,0,1,1,0},
-                {0,0,0,1,1,1},
-                {0,0,1,0,0,0},
-                {0,0,1,0,0,1},
-                {0,0,1,0,1,0},
-                {0,0,1,0,1,1},
-                {0,0,1,1,0,0},
-                {0,0,1,1,0,1},
-                {0,0,1,1,1,0},
-                {0,0,1,1,1,1},
-                {0,1,0,0,0,0}
-               };
-void setup() 
+void setup()
 {
-    Serial.begin(baudrate);
-    while(!Serial){
-      ;  
-    }
-    mySerial.begin(baudrate);
+  Wire.begin();
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+  Serial.begin(baudrate);
+  mySerial.begin(baudrate);
+  isLinked = false;
 }
- 
+
 void loop()
 {
-  if(mySerial.available()){
-    int buf[6] = getData();
-    mySerial.write(buf,6);
+  if(isLinked){
+    Wire.beginTransmission(MPU_addr);
+    Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+    Wire.endTransmission(false);
+    Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
+    AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
+    AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+    AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+    Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+    GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+    GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+    GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+    SendData();
   }
+
+  if (mySerial.available()) {
+    Serial.println(mySerial.read());
+    isLinked = true;
+  }
+
+  delay(100);
 }
 
-public int[] getData(){
-  int datum[6];
-  for(int m = 0; m < 6; m++){
-    datum[m] = data[i][m];
-  }
-  return datum;
+void SendData(){
+  String data = String(AcX) + "," + String(AcY) + "," + String(AcZ) + "_";
+  int bufLength = data.length() + 1;
+  char dataBuff[bufLength];
+  data.toCharArray(dataBuff, bufLength);
+  mySerial.write(dataBuff,bufLength);
 }
+
+
--- Python/BluetoothSetup.py
+++ Python/BluetoothSetup.py
@@ -1,31 +1,30 @@
-import bluetooth
 import sys
 import serial
+import csv
 
-def bluez():
-    count = 0
-    port = 1
-
-    nearby_devices = bluetooth.discover_devices() 
-    for bdaddr in nearby_devices:
-        count += 1
-        print count, ". ", bluetooth.lookup_name( bdaddr )  
-
-    selection = input("> ") - 1
-    print "You have selected", bluetooth.lookup_name(nearby_devices[selection])
-    bd_addr = nearby_devices[selection]
-
-    sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-    sock.connect((bd_addr,port))
-    sock.send("Hello!")
-    sock.close()
 
 ser = serial.Serial("COM11",9600)
 print "Connected"
 ser.write("Ready for data")
 print "Sent Connection Call"
-cats = []
-for i in range(0,5):
-    cats.append(ser.read(1))
 
-print ''.join(cats)
+try:
+    recordedData = []
+    while 1:
+        dataIn = []
+        val = ser.read(1)
+        while val != '_':
+            dataIn.append(val)
+            val = ser.read(1)
+
+        parsedData = ''.join(dataIn)
+        recordedData.append(parsedData)
+        print parsedData
+         
+except KeyboardInterrupt:
+    with open('IMUData.csv','wb') as file:
+        for line in recordedData:
+            file.write(line)
+            file.write('\n')
+
+        file.close()
